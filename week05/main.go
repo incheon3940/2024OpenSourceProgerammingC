ab55a47c2b31f5b827a65aee3d2adaa2fd0e8b49
incheon <incheon3940@gmail.com>
2024-09-27 15:18:10 +0900

    v0.9

--- week05/main.go
+++ week05/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, 안녕")
+
+	var f float32 = 3.14
+
+	///var i int = 33
+	///i = 55
+
+	i := 55
+	fmt.Println(f, math.Ceil((3.14)))
+	fmt.Println(strings.Title("Kim inha"))
+	fmt.Printf("i는 %d\n", i)
+	fmt.Print("i는 ", i, "\n")
+	fmt.Println("i는", i)
 }
