f8fe29f22086e836e1c556f6ebd8d26118e1b168
incheon <incheon3940@gmail.com>
2024-10-04 15:04:13 +0900

    v0.6 Unicode

--- main.go
+++ main.go
@@ -8,8 +8,12 @@ import (
 func main() {
 	i := 13              //var i int = 13
 	var f float64 = 12.9 //f := 12.9
+	c1 := 'Z'            // 90
+	c2 := '김'            // 44608
+
 	fmt.Printf("vlaue i  : %d, valuef %f \n", i, f)
 	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
 	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+	fmt.Println(c1, c2)
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
 }
