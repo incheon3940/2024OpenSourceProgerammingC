088c4dc6303921acd8126183cc16243188628507
incheon <incheon3940@gmail.com>
2024-10-11 15:30:25 +0900

    v1.3 Parse

--- week07/main.go
+++ week07/main.go
@@ -5,15 +5,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	fmt.Print("Input your score : ")
 	in := bufio.NewReader(os.Stdin)
-	fmt.Print("Input youe name : ")
-	name, err := in.ReadString('\n')
+	i, err := in.ReadString('\n')
+
 	if err != nil {
 		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)
+	score, _ := strconv.ParseInt(i, 16, 32)
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Println("%d \n", score)
 	} else {
-		fmt.Println(name)
+		fmt.Println("BCDF")
+		fmt.Println("%d \n", score)
 	}
 }
