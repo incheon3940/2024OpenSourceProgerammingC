b0615881a1dd17ced29a765ba1d24d46a4dd29d0
incheon <incheon3940@gmail.com>
2024-10-11 14:00:58 +0900

    v0.9 Time

--- week07/main.go
+++ week07/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	var now time.Time = time.Now()
-	var month int = int(now.Month()) // var month time.Month = now.Month()
-	fmt.Println(month)
+	fmt.Printf("오늘은 %d년 %d월 %d일 입니다. \n", now.Year(), int(now.Month()), now.Day())
+	fmt.Printf("지금은 시각은 %d시 %d분 %d초 입니다. \n", now.Hour(), now.Minute(), now.Second())
+
 }
