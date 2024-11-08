106829a2b99aeb772fd1d2237fb5ad60cb115509
incheon <incheon3940@gmail.com>
2024-11-08 14:12:08 +0900

    v3.2 Prime Number

--- week11/main.go
+++ week11/main.go
@@ -13,6 +13,7 @@ func isPrime(n int) bool {
 	if n <= 1 {
 		return false
 	} else if n == 2 {
+		return true
 	} else if n%2 == 0 {
 		return false
 	} else {
@@ -21,36 +22,32 @@ func isPrime(n int) bool {
 			if n%j == 0 {
 				return false
 			}
-			fmt.Printf("%d ", j)
-			j += 2
+			//fmt.Printf("%d ", j)
+			j = j + 2
 		}
 	}
 	return true
 }
 
-func main() {
-	fmt.Print("Input Start Number : ")
+func getInteger() int {
 	in := bufio.NewReader(os.Stdin)
 	a, err := in.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
 	a = strings.TrimSpace(a)
-	n1, err := strconv.Atoi(a)
+	n, err := strconv.Atoi(a)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return n
+}
 
-	fmt.Print("Input EndNumber : ")
-	b, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	b = strings.TrimSpace(b)
-	n2, err := strconv.Atoi(b)
-	if err != nil {
-		log.Fatal(err)
-	}
+func main() {
+	fmt.Print("Input start number : ")
+	n1 := getInteger()
+	fmt.Print("Input end number : ")
+	n2 := getInteger()
 
 	for j := n1; j <= n2; j++ {
 		if isPrime(j) {
